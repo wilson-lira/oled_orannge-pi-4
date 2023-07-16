7dafff538c671a6f41fb2d0b7a016a9de1769a6b
WillsonLira <willsonlyra@gmail.com>
2023-07-16 13:48:28 -0300

    oled utility v1

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+
+use std::env;
+use linux_embedded_hal::I2cdev;
+
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use embedded_graphics::{
+    mono_font::{
+        MonoTextStyleBuilder,
+        ascii::FONT_6X10
+    },
+    text::{
+        Text,
+        Baseline
+    },
+    prelude::Point,
+    Drawable,
+    pixelcolor::BinaryColor
+};
+
+fn main() {
+
+    
+
+    let args: Vec<String> = env::args().collect();
+    
+    let line1 = &args[1];
+    let line2 = &args[2];
+
+
+
+    let i2c = I2cdev::new("/dev/i2c-8").unwrap();
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(
+        interface,
+        DisplaySize128x32,
+        DisplayRotation::Rotate0,
+    ).into_buffered_graphics_mode();
+
+    display.init().unwrap();
+    
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+    
+    Text::with_baseline(&line1, Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    
+    Text::with_baseline(&line2, Point::new(0, 14), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    
+    display.flush().unwrap();
+
+}
+
